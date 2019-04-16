f383314f602d7ac80c74722c186ceef9c7958e53
André Habib Fonseca Montenegro <andrehabib@A202-07L.imd.local>
2019-04-16 13:55:01 -0300

    template

--- TEMPLATES/conjunto.cpp
+++ TEMPLATES/conjunto.cpp
@@ -5,33 +5,42 @@ Conjunto<T>::Conjunto(void){
 }
 
 template<typename T>
-void Conjunto<T>::adicionar(T elemento) {
-	listaConjuntos.push_back(elemento);
-	std::cout << "Elemnto: " << listaConjuntos << "foi adicionado !" << std::endl;
+void Conjunto<T>::adicionar(Conjunto &novoConjunto) {
+	for (auto& row : listaConjuntos)
+	{
+		this->listaConjuntos.push_back(row);
+		std::cout << "Elemento: " << this->listaConjuntos.push_back(row) << "foi adicionado !" << std::endl;
+	}
+	
 	return;
 }
 
 T Conjunto<T>::adicionar(T elemento) {
-	for (auto &listaConjuntos : )
-	{
-		/* code */
-	}
-	listaConjuntos.push_back(elemento);
-	std::cout << "Elemnto: " << listaConjuntos << "foi adicionado !" << std::endl;
+	
+	this->listaConjuntos.push_back(elemento);
+	char elemento_ = this->listaConjuntos.push_back(elemento);
+	std::cout << "Elemento: " << elemento_ << "foi adicionado !" << std::endl;
 	return;
 }
 
 emplate<typename T>
 void Conjunto<T>::imprimir() {
 
-	std::cout << "Elemnto: " << listaConjuntos << "foi adicionado !" << std::endl;
+	std::cout << "Elementos do Conjunto: [ ";
+
+	for(auto &row : this->pool) {
+		std::cout << "Elemnto: " << row << "foi adicionado !" << std::endl;
+	}
+
+	std::cout << " ]" << std::endl;
+
 	return;
 }
 
 template<class T>
 int Conjunto<T>::total() {
 	
-	return listaConjuntos.size();
+	return this->listaConjuntos.size();
 }
 
 void Conjunto::Conjunto operator+(Conjunto &a){
